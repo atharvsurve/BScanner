28be674422b69d46a51ecaba1df903a8614e029c
atharv surve <surveatharv1604@gmail.com>
2025-04-30 09:54:21 +0530

    mongo uri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://surveatharv1604:byN3e4b08clQnaMb@cluster0.jgcm7tm.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
